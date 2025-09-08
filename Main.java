70b18c30a0bf1ac76072372f2af5407ce2c49736
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-09-08 10:15:23 -0700

    Added Note constructor and some get and set methods

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println('A' + Note.flat_sym());
-    System.out.println('B' + Note.natural_sym());
-    System.out.println('C' + Note.sharp_sym());
+    Note n = new Note();
+    System.out.println(n.get_name());
+    System.out.println(n.get_letter());
   }
 }
--- Note.java
+++ Note.java
@@ -1,10 +1,19 @@
 public class Note {
   private String name;
-  private char letter;
-  private char accidental;
+  private String letter;
+  private String accidental;
   private int octave;
-  private int num;
+  private int pitch_class;
   private int midi_value;
+
+  public Note() {
+    letter = "C";
+    accidental = natural_sym();
+    octave = 4;
+    pitch_class = 0;
+    midi_value = 60;
+    set_name(letter, accidental, octave);
+  }
 
   public static String flat_sym() {
     return Character.toString(0x266D);
@@ -17,4 +26,21 @@ public class Note {
   public static String sharp_sym() {
     return Character.toString(0x266F);
   }
+
+  public String get_name() {
+    return name;
+  }
+
+  private void set_name(String letter, String accidental, int octave) {
+    if (accidental.equals(natural_sym())) {
+      name = letter + octave;
+    } else {
+      name = letter + accidental + octave;
+    }
+  }
+
+  public String  get_letter() {
+    return letter;
+  }
+
 }
